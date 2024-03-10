ec145d771654efc2443334eac6a7240f8bb4d443
Luiz Moura <muniz.mourafelipe@gmail.com>
2024-03-10 09:48:54 -0300

    Remove initial state from the shared state

--- src/state.ts
+++ src/state.ts
@@ -1,6 +1,5 @@
 import { BehaviorSubject } from "rxjs";
 import { RecipeWithInformation } from "./types/Recipe";
-import initalState from './stateTest.json';
 
-let recipeState: RecipeWithInformation = initalState as any
+let recipeState: RecipeWithInformation = undefined
 export const state = new BehaviorSubject({ recipe: recipeState })
