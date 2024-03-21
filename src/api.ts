472446d0bcf9fde314c6f85876667584d7a10bc5
Luiz Moura <muniz.mourafelipe@gmail.com>
2024-03-21 10:23:38 -0300

    Add news api

--- src/api.ts
+++ src/api.ts
@@ -1,32 +1,53 @@
 import axios from "axios";
+import NewsAPI from "newsapi"
 import { toQueryString } from "./utils/querystring";
 import { SearchRecipesParams } from "./types/SearchRecipesParams";
 import { BaseRecipe, RecipeWithInformation, RecipeWithIngredients } from "./types/Recipe";
 import { Autocomplete } from "./types/Autocomplete";
 
-const api = axios.create({
+const spoonacularApi = axios.create({
   baseURL: "https://api.spoonacular.com",
   headers: {
-    "x-api-key": process.env.API_KEY
+    "x-api-key": process.env.SPOONACULAR_API_KEY
   }
 })
 
+const newsApi = axios.create({
+  baseURL: "https://newsapi.org/v2",
+  headers: {
+    "x-api-key": process.env.NEWS_API_KEY
+  }
+})
+
+const formatDate = (date: Date | undefined): string =>
+  date?.toISOString()?.replace(/T+.*/g, "")
+
+export const news = {
+  everything: async (query: string, offset = 0, number = 5, sortBy = 'publishedAt', from: Date = undefined, to: Date = undefined): Promise<any> =>
+    (await newsApi.get(`/everything${toQueryString({
+      q: query,
+      language: 'en',
+      sortBy,
+      pageSize: number,
+      page: 1 + offset,
+      from: formatDate(from),
+      to: formatDate(to)
+    })}`)).data
+}
+
 export const recipes = {
   getSimilar: async (id: number, number = 10, limitLicense = true): Promise<RecipeWithInformation[]> =>
-    (await api.get(`/recipes/${id}/similar${toQueryString({ number, limitLicense })}`)).data,
+    (await spoonacularApi.get(`/recipes/${id}/similar${toQueryString({ number, limitLicense })}`)).data,
 
   getRandom: async (number = 50, limitLicense = true, includeTags: string[] = [], excludeTags: string[] = []): Promise<RecipeWithInformation[]> =>
-    (await api.get(`/recipes/random${toQueryString({ number, limitLicense, "include-tags": includeTags, "exclude-tags": excludeTags })}`)).data,
+    (await spoonacularApi.get(`/recipes/random${toQueryString({ number, limitLicense, "include-tags": includeTags, "exclude-tags": excludeTags })}`)).data,
 
   getInformation: async (id: number, includeNutrition = false): Promise<RecipeWithInformation> =>
-    (await api.get(`/recipes/${id}/information${toQueryString({ includeNutrition })}`)).data,
-
-  // getAnalyzedInstructions: async (id: number, number = 10, limitLicense: true) =>
-  //   (await api.get(`/recipes/${id}/similar${toQueryString({ number, limitLicense })}`)).data,
+    (await spoonacularApi.get(`/recipes/${id}/information${toQueryString({ includeNutrition })}`)).data,
 
   search: async (query: string, offset = 0, number = 10, params: SearchRecipesParams = null): Promise<(RecipeWithInformation | RecipeWithIngredients | BaseRecipe)[]> =>
-    (await api.get(`/recipes/complexSearch${toQueryString({ query, offset, number, ...params })}`)).data,
+    (await spoonacularApi.get(`/recipes/complexSearch${toQueryString({ query, offset, number, ...params })}`)).data,
 
   autocomplete: async (query: string, number = 10): Promise<Autocomplete[]> =>
-    (await api.get(`/recipes/autocomplete${toQueryString({ query, number })}`)).data,
+    (await spoonacularApi.get(`/recipes/autocomplete${toQueryString({ query, number })}`)).data,
 }
--- src/utils/querystring.ts
+++ src/utils/querystring.ts
@@ -1,4 +1,5 @@
 export const toQueryString = (obj: Record<string, string | number | boolean | string[]>) =>
   Object.entries(obj)
+    .reduce((acc, curr) => curr[1] ? [...acc, curr] : acc, [])
     .map(([key, value], i) => `${i === 0 ? "?" : ""}${key}=${value}`)
     .join("&")
